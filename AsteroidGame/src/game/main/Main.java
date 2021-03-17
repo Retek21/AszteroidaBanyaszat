3ad90e914e6b1596a59e505f1ffdb4e095e99b37
Peti <szeredipeti21@gmai.com>
2021-03-17 13:51:36 +0100

    proba

--- AsteroidGame/src/game/main/Main.java
+++ AsteroidGame/src/game/main/Main.java
@@ -2,14 +2,14 @@ package game.main;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("ÁÁÁÁÁÁÁÁÁÁÁÁÁÁÁÁÁÁÁ");
-        System.out.println("ÁÁ");
-        System.out.println("ÁÁ");
-        System.out.println("ÁÁÁÁÁÁÁÁÁÁÁÁ");
-        System.out.println("ÁÁ");
-        System.out.println("ÁÁ");
-        System.out.println("ÁÁ");
-        System.out.println("ÁÁ");
-        System.out.println("ÁÁ");
+        System.out.println("KAKA-PISA");
+        System.out.println("KAKA-PISA");
+        System.out.println("KAKA-PISA");
+        System.out.println("KAKA-PISA");
+        System.out.println("KAKA-PISA");
+        System.out.println("KAKA-PISA");
+        System.out.println("KAKA-PISA");
+        System.out.println("KAKA-PISA");
+        System.out.println("KAKA-PISA");
     }
 }
