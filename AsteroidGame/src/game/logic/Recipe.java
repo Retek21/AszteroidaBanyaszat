cb6f9ada13f2decd5dcf127102860791719b9a15
Gergő Szabó <szabo.gergo@edu.bme.hu>
2021-03-19 18:05:38 +0100

    Recipe, Factory, Inventory

---
+++ AsteroidGame/src/game/logic/Recipe.java
@@ -0,0 +1,35 @@
+package game.logic;
+
+import java.util.ArrayList;
+
+public class Recipe {
+    public static ArrayList<Material> GetTeleportRecipe() {
+        System.out.println("Recipe: GetTeleportRecipe()\n");
+
+        Iron iron1 = new Iron();
+        Iron iron1 = new Iron();
+        Ice ice = new Ice();
+        Uranium uran = new Uranium();
+
+        ArrayList<Material> mold = new ArrayList<Material>;
+        mold.add(iron1);
+        mold.add(iron2);
+        mold.add(ice);
+        mold.add(uran);
+        return mold;
+    }
+
+    public static ArrayList<Material> GetRobotRecipe() {
+        System.out.println("Recipe: GetRobotRecipe()\n");
+
+        Iron iron = new Iron();
+        Coal coal = new Coal();
+        Uranium uran = new Uranium();
+
+        ArrayList<Material> mold = new ArrayList<Material>;
+        mold.add(iron);
+        mold.add(coal);
+        mold.add(uran);
+        return mold;
+    }
+}
