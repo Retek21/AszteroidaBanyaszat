aaa1036c69aa0e3cd9c2fe767e60f22af8d6b1f4
dengyelbendeguz <79416229+dengyelbendeguz@users.noreply.github.com>
2021-03-21 19:50:50 +0100

    Update Main.java
    asdasd

--- AsteroidGame/src/game/main/Main.java
+++ AsteroidGame/src/game/main/Main.java
@@ -1,11 +1,1 @@
-package game.main;
 
-import game.logic.Asteroid;
-import game.logic.Skeleton;
-
-public class Main {
-    public static void main(String[] args) {
-        Skeleton s = new Skeleton();
-        s.SunstormHitsAsteroidfield();
-    }
-}
