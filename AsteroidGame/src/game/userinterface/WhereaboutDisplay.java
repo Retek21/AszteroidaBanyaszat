3d4b7da70e874a98d504a3ca60e1e6c2c795cd1e
Krinya-byte <torokkristof692@gmail.com>
2021-05-08 17:27:53 +0200

    notify logika only Displayeknel, Display.SetSelected felig bekotve

---
+++ AsteroidGame/src/game/userinterface/WhereaboutDisplay.java
@@ -0,0 +1,14 @@
+package game.userinterface;
+
+public class WhereaboutDisplay extends Display{
+    private boolean isNeighbour;
+
+    public void SetisNeigbhour(boolean neigbhour) {
+        isNeighbour = neigbhour;
+        DisplayManager.GetInstance().DrawDisplays();
+    }
+    public boolean IsNeighbour() {
+        return isNeighbour;
+    }
+    public void NotifyNeighbour(){}
+}
